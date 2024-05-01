812f326868d14af02475ff7b09cda0cd8ebf58e3
AlaminSarkerFRII <alamin.education1997@gmail.com>
2024-05-01 23:58:59 +0600

    solve products showing issues

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,74 @@
 from django.views import generic
+from django.views.generic import ListView
+from product.models import Variant, Product
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView ,CreateView
+from product.forms import ProductForm 
 
-from product.models import Variant
 
 
-class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        filter_string = {}
+        
+        for key, value in self.request.GET.items():
+            if value:
+                filter_string[key] = value
+        
+        queryset = queryset.filter(**filter_string)
+        return queryset
 
     def get_context_data(self, **kwargs):
-        context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context = super().get_context_data(**kwargs)
+        # context['dashboard'] = True
         context['product'] = True
-        context['variants'] = list(variants.all())
+        context['request'] = self.request.GET.get('title__icontains', '')
+        return context
+
+
+
+# class CreateProductView(generic.TemplateView):
+#     template_name = 'products/create.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(CreateProductView, self).get_context_data(**kwargs)
+#         variants = Variant.objects.filter(active=True).values('id', 'title')
+#         context['product'] = True
+#         context['variants'] = list(variants.all())
+#         return context
+
+
+class CreateProductView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/create.html'
+    success_url = reverse_lazy('list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['variants'] = Variant.objects.filter(active=True).values('id', 'title')
+        return context
+
+
+
+class UpdateProductView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/edit.html' 
+    success_url = reverse_lazy('list')
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        # Provide variant options for the form
+        context['variants'] = Variant.objects.filter(active=True).values('id', 'title')
         return context
--- src/product/views/variant.py
+++ src/product/views/variant.py
@@ -1,6 +1,7 @@
 from django.views import generic
-from django.views.generic import ListView, CreateView, UpdateView
-
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView ,CreateView
+from django.urls import reverse_lazy
 from product.forms import VariantForm
 from product.models import Variant
 
@@ -14,28 +15,50 @@ class BaseVariantView(generic.View):
 
 class VariantView(BaseVariantView, ListView):
     template_name = 'variants/list.html'
+    model = Variant
     paginate_by = 10
+    context_object_name = 'variants'
+
 
     def get_queryset(self):
+        queryset = Variant.objects.all()
         filter_string = {}
-        print(self.request.GET)
-        for key in self.request.GET:
-            if self.request.GET.get(key):
-                filter_string[key] = self.request.GET.get(key)
-        return Variant.objects.filter(**filter_string)
+        # print(self.request.GET)
+        # for key in self.request.GET:
+        #     if self.request.GET.get(key):
+        #         filter_string[key] = self.request.GET.get(key)
+        # return Variant.objects.filter(**filter_string)
+
+        for key, value in self.request.GET.items():
+            if value:
+                filter_string[key] = value
+        
+        queryset = queryset.filter(**filter_string)
+        return queryset
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['product'] = True
+    #     context['request'] = ''
+    #     if self.request.GET:
+    #         context['request'] = self.request.GET['title__icontains']
+    #     return context
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['product'] = True
-        context['request'] = ''
-        if self.request.GET:
-            context['request'] = self.request.GET['title__icontains']
+        context['request'] = self.request.GET.get('title__icontains', '')
         return context
 
 
 class VariantCreateView(BaseVariantView, CreateView):
-    pass
+    template_name = 'variants/create.html'
+    form_class = VariantForm
+    success_url = reverse_lazy('variant-list')
 
 
 class VariantEditView(BaseVariantView, UpdateView):
+    template_name = 'variants/edit.html'
+    form_class = VariantForm
     pk_url_kwarg = 'id'
+    success_url = reverse_lazy('variant-list')
