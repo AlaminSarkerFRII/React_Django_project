227400e2a7775a142ff94736eac17d29c999c25a
AlaminSarkerFRII <alamin.education1997@gmail.com>
2024-05-01 22:53:44 +0600

    solve this dataloads in database

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Variant, Product, ProductImage, ProductVariant, ProductVariantPrice
 
-# Register your models here.
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ['title', 'active', 'created_at', 'updated_at']
+    search_fields = ['title']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sku', 'created_at', 'updated_at']
+    search_fields = ['title', 'sku']
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product', 'file_path', 'created_at', 'updated_at']
+    search_fields = ['product__title']
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ['variant_title', 'variant', 'product', 'created_at', 'updated_at']
+    search_fields = ['variant_title', 'variant__title', 'product__title']
+
+@admin.register(ProductVariantPrice)
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = ['product_variant_one', 'product_variant_two', 'product_variant_three', 'price', 'stock', 'product', 'created_at', 'updated_at']
+    search_fields = ['product_variant_one__variant_title', 'product_variant_two__variant_title', 'product_variant_three__variant_title', 'product__title']
